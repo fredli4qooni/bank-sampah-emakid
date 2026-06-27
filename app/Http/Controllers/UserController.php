4404254f81e7352bc290ab683e51481b73452661
fredli4qooni <fredlifourqoni999@gmail.com>
2026-06-27 09:32:12 +0700

    Fix unique email validation to ignore current user by explicitly setting id_user as the primary key column

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,7 +49,7 @@ class UserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,' . $user->id_user],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,' . $user->id_user . ',id_user'],
             'role' => ['required', 'in:admin,penimbang,pengelola'],
         ]);
 
